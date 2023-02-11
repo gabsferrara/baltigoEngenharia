270f188a40c48fa02cc0b6a9f4cf609916757e0e
Gabriel Ferrara <gabrielbarbosa_ferrara@hotmail.com>
2023-02-10 22:22:37 -0300

    finish responsive navbar

--- baltigo-front/src/components/Navbar/index.tsx
+++ baltigo-front/src/components/Navbar/index.tsx
@@ -1,41 +1,67 @@
 import BaltigoCinza from '../../assets/img/logos/Baltigo-Cinza.png'
-import Econt from '../../assets/img/logos/e-Scontorno.png'
-import EBaltigo from '../../assets/img/logos/E-Contorno-min.jpg'
-import Hamburguer from '../../assets/img/logos/menuhamburguer.png'
-import x from '../../assets/img/logos/x.png'
-import style from './Navbar.module.scss'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+import { Link, useNavigate } from "react-router-dom";
+import "./navbar.scss";
 
+function Navbar() {
 
-
-export default function Navbar() {
-
+    const navigate = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [size, setSize] = useState({
+      width: 0,
+      height: 0,
+    });
+    useEffect(() => {
+      const handleResize = () => {
+        setSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+      window.addEventListener("resize", handleResize);
+  
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+  
+    useEffect(() => {
+      if (size.width > 768 && menuOpen) {
+        setMenuOpen(false);
+      }
+    }, [size.width, menuOpen]);
+  
+    const menuToggleHandler = () => {
+      setMenuOpen((p) => !p);
+    };
+  
     return (
-        <>
-            <header className={style.header}>
-                <nav className={style.navPc}>
-                    <Link to='/'><img src={BaltigoCinza}></img></Link>
-                    <ul>
-                        <li><Link to={'/'}>Inicio</Link></li>
-                        <li><Link to={'/Servicos'}>Servicos</Link></li>
-                        <li><Link to={'/SobreNos'}>Quem somos nós</Link></li>
-                        <li><Link to={'/Contato'}>Contato</Link></li>
-                    </ul>
-                </nav>
-                <nav className={style.navMobile}>
-                    <Link to='/'><img src={EBaltigo}></img></Link>
-                    <div className={style.menu}>
-                        <img src={Hamburguer} className= {style.icon}></img>
-                        <img src={x} className= {style.icon}></img>
-                        <ul>
-                            <li><Link to={'/'}>Inicio</Link></li>
-                            <li><Link to={'/Servicos'}>Servicos</Link></li>
-                            <li><Link to={'/SobreNos'}>Quem somos nós</Link></li>
-                            <li><Link to={'/Contato'}>Contato</Link></li>
-                        </ul>
-                    </div>
-                </nav>
-            </header>
-        </>
-    )
-}
+      <header className="header">
+        <div className="header__content">
+        <Link to='/'><img src={BaltigoCinza}></img></Link>
+          <nav
+            className={`${"header__content__nav"} 
+            ${menuOpen && size.width < 768 ? `${"isMenu"}` : ""} 
+            }`}
+          >
+            <ul>
+                <li><Link to={'/'}>Inicio</Link></li>
+                <li><Link to={'/Servicos'}>Servicos</Link></li>
+                <li><Link to={'/SobreNos'}>Quem somos nós</Link></li>
+                <li><Link to={'/Contato'}>Contato</Link></li>
+            </ul>
+          </nav>
+          <div className="header__content__toggle">
+            {!menuOpen ? (
+              <BiMenuAltRight onClick={menuToggleHandler} />
+            ) : (
+              <AiOutlineClose onClick={menuToggleHandler} />
+            )}
+          </div>
+        </div>
+      </header>
+    );
+  }
+
+  export default Navbar;
+
